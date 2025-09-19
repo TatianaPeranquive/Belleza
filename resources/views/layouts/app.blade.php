b7715d97ebe2f2bc2c650852a03db4e754f5ff7e
Anggy Tatiana Peranquive Gomez <tatianaperanquive@gmail.com>
2025-09-19 11:24:53 -0500

    Fusion welcome con mi_espejito

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,37 +3,67 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Proyecto Espejo')</title>
+
     @vite('resources/css/app.css')
 </head>
 <body class="{{ $theme === 'dark' ? 'bg-[#111827] text-[#F9FAFB]' : 'bg-white text-[#111827]' }}">
     <header class="fixed top-0 left-0 w-full bg-black z-50">
     <div class="flex justify-between items-center p-4 text-white">
-        <div class="font-bold text-lg">Proyecto Espejo</div>
-        <button id="menuButton" class="text-white font-bold px-4 py-2 border rounded">MENU</button>
+        <div class="font-bold text-lg"></div>
+        <div class="flex justify-between items-center p-4 text-white"></div>
+                <nav id="menu"
+                    class="fixed top-0 left-0 w-full z-50 opacity-0 pointer-events-none transition-all duration-300">
+                    <ul class="flex justify-center gap-4 w-full max-w-6xl mx-auto">
+
+                        <li class="flex-1">
+                            <a href="#home"
+                                class="block w-full text-center  px-4 py-2  rounded border font-bold text-lg hover:underline">
+                                Espejito, espejito
+                            </a>
+                        </li>
+                        <li class="flex-1">
+                            <a href="#work"
+                                class="block w-full text-center  px-4 py-2  rounded border font-bold text-lg hover:underline">
+                                Entramado
+                            </a>
+                        </li>
+                        <li class="flex-1">
+                            <a href="#contact"
+                                class="block w-full text-center px-4 py-2  rounded border font-bold text-lg hover:underline">
+                                Salón de espejos
+                            </a>
+                        </li>
+                        <li class="flex-1">
+                            <a href="#about"
+                                class="block w-full text-center  px-4 py-2  rounded border font-bold text-lg hover:underline">
+                                Dentro del espejo
+                            </a>
+                        </li>
+                        <li class="flex-1">
+                            <a href="#about2"
+                                class="block w-full text-center  px-4 py-2  rounded border font-bold text-lg hover:underline">
+                                Detrás del espejo
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+</div>
     </div>
-    <nav id="menuPanel" class="hidden bg-black text-white text-center absolute top-full left-0 w-full">
-        <ul class="flex flex-col gap-4 py-6">
-            <li><a href="{{ route('home') }}" class="text-xl hover:underline">Inicio</a></li>
-            <li><a href="{{ route('entrevistas.index') }}" class="text-xl hover:underline">Entrevistas</a></li>
-            <li><a href="#" class="text-xl hover:underline">Llenar tu Espejo</a></li>
-            </li><a href="{{ route('detras.many', ['ids' => '11,12']) }} " class="text-xl hover:underline">Detrás del espejo</a></li>
-        </ul>
-    </nav>
+
 </header>
 
  <main class="min-h-screen">
     @yield('content')
 </main>
 
-<div class="fixed bottom-4 right-4 bg-black p-4 rounded text-white shadow-lg">
+<!-- <div class="fixed bottom-4 right-4 bg-black p-4 rounded text-white shadow-lg">
   <a href="{{ route('theme.switch','light') }}"
      class=" px-3 py-1 rounded border text-white font-bold text-lg hover:underline"
      title="Claro">Claro</a>
   <a href="{{ route('theme.switch','dark') }}"
      class="px-3 py-1 rounded border text-gray font-bold text-lg hover:underline"
      title="Oscuro">Oscuro</a>
-</div>
+</div> -->
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js"></script>
 <script>
