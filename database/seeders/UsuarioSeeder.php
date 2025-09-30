f8bf569599b5891a67d7a5c7a68a8256c09a66ce
Anggy Tatiana Peranquive Gomez <tatianaperanquive@gmail.com>
2025-09-29 22:21:07 -0500

    ajusstes a seeders y columnas de la tabla usuarios de la base de daatos

--- database/seeders/UsuarioSeeder.php
+++ database/seeders/UsuarioSeeder.php
@@ -1,13 +1,33 @@
 <?php
+
 namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-class UsuarioSeeder extends Seeder {
-    public function run() {
+
+
+class UsuarioSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
         DB::table('usuarios')->insert([
-            ['nombre'=>'Tatiana','oficio'=>'Diseñadora','locacion'=>'Bogotá','edad'=>30],
-            ['nombre'=>'Ana','oficio'=>'Ingeniera','locacion'=>'Medellín','edad'=>28],
-           ['nombre' => 'María Fernanda', 'oficio' => 'Abogada', 'locacion' => 'Cali', 'edad' => 35]
+            ['nombre' => 'María Fernanda Fitzgerald', 'oficio' => 'Periodista', 'locacion' => 'Bogotá', 'edad' => 0],
+            ['nombre' => 'Diana Bonnett', 'oficio' => 'Historiadora', 'locacion' => 'Medellín', 'edad' => 0],
+            ['nombre' => 'Carol Figeroa', 'oficio' => 'Periodista', 'locacion' => 'Bogotá', 'edad' => 0],
+            ['nombre' => 'Paola Turbay', 'oficio' => 'Actriz', 'locacion' => 'Bogotá', 'edad' => 0],
+            ['nombre' => 'Lucia Meneses', 'oficio' => 'Antropóloga', 'locacion' => '', 'edad' => 0],
+            ['nombre' => 'Melissa Jiménez', 'oficio' => 'Geógrafa', 'locacion' => 'Cali', 'edad' => 0],
+            ['nombre' => 'Natalie', 'oficio' => 'Chef', 'locacion' => 'Bogotá', 'edad' => 0],
+            ['nombre' => 'Diana Suaza', 'oficio' => 'Antropóloga', 'locacion' => 'Bogotá', 'edad' => 0],
+            ['nombre' => 'Himelda Estupiñán', 'oficio' => 'Vendedora Amway', 'locacion' => 'Duitama', 'edad' => 0],
+            ['nombre' => 'Mariana', 'oficio' => 'Estudiante-blogger', 'locacion' => 'Bogotá', 'edad' => 0],
+            ['nombre' => 'Angélica Blanco', 'oficio' => 'Historiadora', 'locacion' => 'Bogotá', 'edad' => 0],
+            ['nombre' => 'Anggy Peranquive Gómez', 'oficio' => 'Ingeniera Electrónica', 'locacion' => 'Bogotá', 'edad' => 29]
+
         ]);
     }
 }
