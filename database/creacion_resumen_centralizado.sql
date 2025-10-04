2c181aae226160fe5017a78b88065d20aad57095
Anggy Tatiana Peranquive Gomez <tatianaperanquive@gmail.com>
2025-10-03 21:19:25 -0500

    consultas SQL a tabla ResumenFiltro

---
+++ database/creacion_resumen_centralizado.sql
@@ -0,0 +1,10 @@
+CREATE TABLE resumen_centralizado(
+  id bigserial,
+  usuario text,
+  comentario text,
+  categoria_1 text,  -- Hito
+  categoria_2 text,  -- Subnivel 1
+  categoria_3 text,  -- Subnivel 2
+  categoria_4 text  -- Subnivel 3 (opcional)
+  
+)
