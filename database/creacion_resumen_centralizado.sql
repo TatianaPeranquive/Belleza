7aafef385c80aa81da396dad5bd13a1d93ac4b4a
Anggy Tatiana Peranquive Gomez <tatianaperanquive@gmail.com>
2025-11-16 00:21:03 -0500

    Activar Nivel 4 en categorias de Hitos OK

--- database/creacion_resumen_centralizado.sql
+++ database/creacion_resumen_centralizado.sql
@@ -5,6 +5,7 @@ CREATE TABLE resumen_centralizado(
   categoria_1 text,  -- Hito
   categoria_2 text,  -- Subnivel 1
   categoria_3 text,  -- Subnivel 2
-  categoria_4 text  -- Subnivel 3 (opcional)
-  
+  categoria_4 text,  -- Subnivel 3 (opcional)
+  categoria_5 text  -- Subnivel 4
+
 )
